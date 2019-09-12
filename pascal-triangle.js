4e3524b41816471e489282a400902f1aac2a7a53
Mozart Diniz <mozart.diniz@gamesys.co.uk>
2019-09-12 09:42:11 +0100

    pascal triangle

---
+++ pascal-triangle.js
@@ -0,0 +1,20 @@
+const pascalTriangle = (numRows) => {
+    const triangle = [];
+
+    for (let i = 0; i < numRows; i++) {
+        const rowSize = i + 1;
+        triangle[i] = new Array(rowSize);
+
+        for(let j = 0; j < rowSize; j++) {
+            if (j === 0 || j === i) {
+                triangle[i][j] = 1;
+            } else {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+    }
+
+    return triangle;
+};
+
+console.table(pascalTriangle(6));
